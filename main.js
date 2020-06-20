8a41831a54494e9bf507a5b557a8a42e427ec91a
Ramer357 <eric.dragos007@gmail.com>
2020-06-20 15:11:26 +0300

    First Commit

--- main.js
+++ main.js
@@ -19,10 +19,9 @@ client.once('message', async message=>{
 
     let args = message.content.slice(PREFIX.length).split(" ");
 
-    switch(args[0]){
         //embed
-            case 'info':
-            const embed = new Discord.MessageEmbed()
+            if(message.content === '^info'){
+              const embed = new Discord.MessageEmbed()
             .setTitle('Basic Information')
             .addField('Your Name', message.author.username)
             .addField('Owner of the BOT', owner)
@@ -31,51 +30,20 @@ client.once('message', async message=>{
             .setColor(0x00A1FF)
             .setThumbnail(message.author.avatarURL)
             message.channel.send(embed);
-            break;
-        case 'salut':
+            }
+            
+        if(message.content === '^salut'){
             message.reply(' Salut bro');
-            break;
+        }
         
-        case 'tempmute':
-        let person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]))
-        if (!person){
-            return message.reply("That member is not in your discord server!");
-        }
 
-        let mainrole = message.guild.roles.find(role => role.name === "Member");
-        let muterole = message.guild.roles.find(role => role.name === "Muted");
-
-        if (!muterole){
-            return message.channel.send("Couldn't find the mute role! ")
-        }
-
-        let time = args[2];
-
-        if (!time){
-            return message.channel.send("You didn't specify a time! ")
-        }
-
-        person.roles.remove(mainrole.id);
-        person.roles.add(muterole.id);
-
-        message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`);
-
-        setTimeout(function(){
-            person.roles.add(mainrole.id);
-            person.roles.remove(muterole.id);
-            message.channel.send(`@${person.user.tag} has been unmuted!`)
-        }, ms(time))
-
-
-        break;
-        case 'zenith':
+        if (message.content === '^zenith'){
             message.channel.send('Zeni e nab si sclavu lu ramer lol haha salamtroll');
-            break;
-        case 'avatar':
-            message.channel.send(message.author.displayAvatarURL());
-            break;
-        case 'erweck':
-            const embedErweck = new Discord.MessageEmbed()
+        }
+        if(message.content === '^avatar'){
+            message.channel.send(message.author.displayAvatarURL());}
+        if (message.content === '^erweck'){
+          const embedErweck = new Discord.MessageEmbed()
             .setTitle('Erweck sucks lol salamtroll haha')
             .setColor(0xFC00FF)
             .addField('El a scris comanda', message.author.username + ' e vina lui nu a mea')
@@ -84,24 +52,22 @@ client.once('message', async message=>{
             .addField('Esti naber', 'Ai bulan si esti si nici cu ala nu poti sa faci peste 3 killuri');
             message.channel.send(embedErweck);
             message.channel.send('joke nu te supara lol');
+        }
         
-            break;
-        case 'ramer':
+        if(message.content === '^ramer')
             message.channel.send(`!kick @Erweck#5109`);
-            break;
-        case 'zrelax':
+        if(message.content === '^zrelax'){
             const embedRelax = new Discord.MessageEmbed()
             .setTitle('zRelax e sclavul lui ' + message.author.username)
             .setColor(0xFC00FF)
             .addField('Erweck e suparat', 'dovezile sunt ca relax l-a batut si nu voia asta');
             message.channel.send(embedRelax);
+        }
 
-            break;
-        case 'tali':
+        if(message.content === '^tali')
             message.channel.send('ma pis pe voi frate ca jucati roblox ma pis in voi erweck si relax sunt prostiii');
-          break;
 
-        case 'help':
+        if(message.content === '^help'){
             const embedHelp = new Discord.MessageEmbed()
             .setTitle('Help Commands')
             .setColor(0x00A1FF)
@@ -110,19 +76,17 @@ client.once('message', async message=>{
             .addField('Comenzi Diverse', 'avatar')
             .addField('Comenzi de Moderare', 'ban ' + 'kick ');
             message.channel.send(embedHelp);
+        }
 
-            break;
-
-        case 'prefix':
+        if(message.content === '^prefix'){
           const embedPrefix = new Discord.MessageEmbed()
           .setTitle('Prefixul meu este ^')
           .setColor(0x00A1FF);
           message.channel.send(embedPrefix);
-
-          break;
+        }
         //Moderation coms:
 
-        case 'kick':
+        if(message.content === '^kick'){
           if(!message.member.roles.cache.find(r => r.name === "Baieti Smecheri")) return message.channel.send('Nu ai permisiuni de a da kick!')
           if (!args[1]) message.channel.send('Trebuie sa ii dai tag celui caruia vrei sa-i dai kick!')
 
@@ -145,11 +109,10 @@ client.once('message', async message=>{
           }else {
             message.reply('Acel utilizator nu este in acest server!')
           }
+        }
 
-          break;
-
-        case 'ban':
-          if(!message.member.roles.cache.find(r => r.name === "Baieti Smecheri")) return message.channel.send('Nu ai permisiuni de a da ban!')
+        if (message.content === '^ban')
+          if(!message.member.roles.cache.find(r => r.name === "Baieti Amuzanti")) return message.channel.send('Nu ai permisiuni de a da ban!')
           if (!args[1]) message.channel.send('Trebuie sa ii dai tag celui caruia vrei sa-i dai ban!')
 
           const userb = message.mentions.users.first();
@@ -176,20 +139,17 @@ client.once('message', async message=>{
             message.reply('Acel utilizator nu este in acest server!')
           }
 
-          break;
-
-          case 'owner':
+          if(message.content === '^owner'){
             const embedOwner = new Discord.MessageEmbed()
             .setTitle('Ownerul meu este </ramer>#9999');
 
             message.channel.send(embedOwner);
-            break;
 
     }
 
     
 
-});
+  })
 
 client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
