d0eec1d2115977d48faec261be514aafd4fde557
Ramer357 <eric.dragos007@gmail.com>
2020-06-22 15:53:03 +0300

    asd

--- main.js
+++ main.js
@@ -68,10 +68,10 @@ client.on('message', async message => {
     .setTitle ('Tali$ - Diss Tali')
     .setColor ('0xFF0000')
     .addField ('Tali, ye ye ye', 'Esti prost, tali, sau prost')
-    .addField ('', 'prost!');
+    .addField('prost!', 'o ye');
 
     message.channel.send (embedTali);
-    message.channel.send ('Ti-am dat-o rau de tot tali lasa-te de viata si du-te sa plangi intr-un colt te rog ahaha');
+    message.channel.send ('Ti-am dat-o rau de tot tali lasa-te de viata si du-te sa plangi intr-un colt si da-ti alt+f4 la viata ba hahaha');
 
   }
 
